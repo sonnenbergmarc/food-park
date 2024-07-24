c3bb199f9c9ca41e42a39cd9b0deef32ae0d4da9
Marcus <msonnenberg@live.com>
2024-07-24 09:56:56 -0600

    Added create feature for product. Created a global helper php file for the slug to be used in other pages if it needs to be. Tested the product create feature and it works.

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\DataTables\ProductDataTable;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\ProductCreateRequest;
+use App\Models\Category;
+use App\Models\Product;
+use App\Traits\FileUploadTrait;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Str;
 
 class ProductController extends Controller
 {
+    use FileUploadTrait;
     /**
      * Display a listing of the resource.
      */
@@ -19,17 +26,38 @@ class ProductController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create() : View
     {
-        //
+        $categories = Category::all();
+        return view('admin.product.create', compact('categories'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ProductCreateRequest $request)
     {
-        //
+        /** Handle Image File */
+        $imagePath = $this->uploadImage($request, 'image');
+
+        $product = new Product();
+        $product->thumb_image = $imagePath;
+        $product->name = $request->name;
+        $product->slug = generateUniqueSlug('Product', $request->name);
+        $product->category_id = $request->category;
+        $product->price = $request->price;
+        $product->offer_price = $request->offer_price;
+        $product->short_description = $request->short_description;
+        $product->long_description = $request->long_description;
+        $product->sku = $request->sku;
+        $product->seo_title = $request->seo_title;
+        $product->seo_description = $request->seo_description;
+        $product->show_at_home = $request->show_at_home;
+        $product->status = $request->status;
+        $product->save();
+
+        toastr()->success('Successfully Created Product!');
+        return to_route('admin.product.index');
     }
 
     /**
--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -14,6 +14,8 @@
     <link rel="stylesheet" href="{{ asset('admin/assets/css/toastr.min.css') }}">
     <link rel="stylesheet" href="//cdn.datatables.net/2.0.8/css/dataTables.dataTables.min.css">
     <link rel="stylesheet" href="{{ asset('admin/assets/css/bootstrap-iconpicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/modules/select2/dist/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/modules/summernote/summernote-bs4.css') }}">
 
     <!-- Template CSS -->
     <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
@@ -71,6 +73,8 @@
 
     <script src="//cdn.datatables.net/2.0.8/js/dataTables.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="{{ asset('admin/assets/modules/select2/dist/js/select2.full.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/summernote/summernote-bs4.js') }}"></script>
 
     <!-- Template JS File -->
     <script src="{{ asset('admin/assets/js/scripts.js') }}"></script>
