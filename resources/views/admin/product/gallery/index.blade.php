432b875d5f37990c0df12d3ece509ea50f3497ea
Marcus <msonnenberg@live.com>
2024-07-25 09:39:08 -0600

    Created a product gallery controller, model and migration. Also, added functionality to the extra button in the product page which includes the Product Gallery which takes you to the product gallery index page.

---
+++ resources/views/admin/product/gallery/index.blade.php
@@ -0,0 +1,22 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Product Gallery</h1>
+        </div>
+        <div class="card card-primary">
+            <div class="card-header">
+                <h4>All Products</h4>
+                <div class="card-header-action">
+                    <a href="{{ route('admin.product.create') }}" class="btn btn-primary">
+                        Create New
+                    </a>
+                </div>
+            </div>
+            <div class="card-body">
+
+            </div>
+        </div>
+    </section>
+@endsection
